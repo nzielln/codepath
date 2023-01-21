3784230c32f26b1a7b1da8c4431307a0455c1ed5
Ellah Nzikoba <ellahnzikoba@gmail.com>
2023-01-21 17:38:42 -0500

    Initial Commit

---
+++ IntroduceStudent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroduceStudent
+//
+//  Created by Ellah Nzikoba on 1/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
